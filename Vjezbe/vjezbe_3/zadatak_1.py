e5730e3b44f19e1ac21c01733ced1d16453e05bc
unknown <iprkut@pmfst.hr>
2025-04-14 20:50:18 +0200

    zadatak 1

---
+++ Vjezbe/vjezbe_3/zadatak_1.py
@@ -0,0 +1,10 @@
+rezultat=5.0-4.935
+print(rezultat)
+
+#Očekujemo kao rezultat 0.065,takav rezultat ne dobijemo jer se broj 4.935 ne može točno binarno prikazati
+
+
+zbroj=0.1+0.2+0.3
+print(zbroj)
+print(0.1 + 0.2 + 0.3 == 0.6)
+
