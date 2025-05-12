2eb5bc1f7af7b8b699a63eb6ac817268b3e4c47e
unknown <iprkut@pmfst.hr>
2025-05-12 20:36:36 +0200

    Vjezbe

---
+++ Vjezbe/vjezbe_6/zadatak1.py
@@ -0,0 +1,10 @@
+from harmonijskioscilator import HarmonicOscillator
+
+
+if __name__ == "__main__":
+    oscilator = HarmonicOscillator(m=1.0, k=1.0, x0=1.0, v0=0.0, dt=0.01)
+    oscilator.simulacija(t_max=20)
+    oscilator.graf()
+    period = oscilator.izracunaj_period()
+    print('period oscilacije: ',period)
+    
